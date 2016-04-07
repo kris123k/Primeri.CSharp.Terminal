92dc3dc236beabe9863d15058764be46fa5a8c3a
kris123k <kriskirkov1995@gmail.com>
2016-04-07 15:39:09 +0300

    modul 2 dobavqne na komentari

---
+++ modul02/modul02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//definirane na promenlivi
+
+
+
+			//subirane na chisla
+
+
+
+			//pisane v konzolata
+		}
+	}
+}
