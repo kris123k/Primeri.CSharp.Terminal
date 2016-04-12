2695d886044f3d241a3cb71440a0c66c68ac9cad
kris123k <kriskirkov1995@gmail.com>
2016-04-12 16:39:18 +0300

    modul 02, video 02 - promenlivi int

--- modul02/modul02/Program.cs
+++ modul02/modul02/Program.cs
@@ -8,14 +8,25 @@ namespace modul02
 		{
 
 			//definirane na promenlivi
-
+			int a = 0, b = 0; 
+			int sum = 0;	
 
 
 			//subirane na chisla
+			a = 5; b = 32; 
+			sum = a + b;
 
+		
 
 
 			//pisane v konzolata
+			Console.Write (a);
+			Console.Write ("+");
+			Console.Write (b);
+			Console.Write ("=");
+			Console.WriteLine (sum);
+
+
 		}
 	}
 }
