7161edf7059dc2af1ea29322c23fa7028de8a45d
kris123k <kriskirkov1995@gmail.com>
2016-04-12 17:23:16 +0300

    modul 02 video 5 - chetene ot red

---
+++ modul02/readfromcommandline/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace readfromcommandline
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// definirane na promenlivi
+			int a = 5, b = 0;
+
+			// vuvejdane na parametri
+			Console.Write ("vuvedete b: ");
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+			// pechat na rezultat
+			Console.WriteLine ("rezultata a+b e: " + (a + b).ToString () + "\n\n\n");
+
+
+		}
+	}
+}
